5ba6e2156a9d0b9a8fdb073cc85ce565c444656d
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-04-23 20:10:24 +0400

    fix[matrix1.js]: Fixing bugs

--- array-methods-implementations-tasks/matrix-tasks/matrix1.js
+++ array-methods-implementations-tasks/matrix-tasks/matrix1.js
@@ -1,31 +1,53 @@
 //1.Rotate any matrix
 const matrix = [
   [1, 2, 3],
-  [4, 5, 6],
+  [4, 5, []],
   [7, 8, 9],
-  [10, 11, 12],
-  [11, 12, 13],
-  [14, 15, 16],
+  [null, 11, 12],
+  [13, 14, NaN],
 ];
-//length-length[0].length
+
 const rotate90Degree = (matrix) => {
   const result = [];
   const length = matrix.length;
-  const count = matrix.length - matrix[0].length;
+  let count;
+  let check;
+
   if (matrix.length !== matrix[0].length && matrix.length > matrix[0].length) {
+    count = matrix.length - matrix[0].length;
+    check = "case1";
+
     matrix.forEach((row, rowIndex) => {
       for (let i = 0; i < count; i++) row.push(null);
     });
   }
+
+  if (matrix.length !== matrix[0].length && matrix.length < matrix[0].length) {
+    count = matrix[0].length - matrix.length;
+    check = "case2";
+    const getNull = () => null;
+    let pushedArr = Array.from({ length: matrix[0].length }, getNull);
+    for (let i = 0; i < count; i++) {
+      matrix.push(pushedArr);
+    }
+  }
+
   matrix.forEach((row, rowIndex) => {
     const temp = [];
+    // debugger;
     row.forEach((column, columnIndex) => {
-      temp.push(matrix[length - columnIndex - 1][rowIndex]);
+      temp.push(matrix[matrix.length - columnIndex - 1][rowIndex]);
     });
     result.push(temp);
   });
-  result.splice(result.length - count);
-  console.log("result", result);
+
+  if (check == "case1") result.splice(result.length - count);
+  if (check == "case2") {
+    result.forEach((row, rowIndex) => {
+      row.splice(0, count);
+    });
+  }
+
   return result;
 };
 
@@ -33,7 +55,6 @@ const calculateRotationNumber = (deg) => {
   if (deg % 90 !== 0) {
     throw new RangeError("Incorrect value");
   }
-
   // ((deg % 360) + 360) % 360 / 90
   deg = (deg % 360) / 90;
   deg += deg < 0 ? 4 : 0;
