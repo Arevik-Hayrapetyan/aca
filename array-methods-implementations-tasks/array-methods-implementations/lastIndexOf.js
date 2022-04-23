e7bfb980658b510203f990c69490016b03114e61
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-04-23 13:10:10 +0400

    feat[lastIndexOf.js]: lastIndexOf implementation

---
+++ array-methods-implementations-tasks/array-methods-implementations/lastIndexOf.js
@@ -0,0 +1,13 @@
+Array.prototype.myLastIndexOf = function (searchElement, fromIndex) {
+  const start = fromIndex ? fromIndex : 0;
+
+  for (let index = this.length; index > start; index--) {
+    if (this[index] === searchElement) {
+      return index;
+    }
+  }
+  return -1;
+};
+const array = [40, 20, 30, 40];
+const answer = array.myLastIndexOf(40);
+console.log(answer);
