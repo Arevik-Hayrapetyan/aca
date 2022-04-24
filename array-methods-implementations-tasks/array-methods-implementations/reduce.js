4df754c49bcfa53cb8f5d1ffa8f77ff0fd2b8d0f
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-04-24 23:40:05 +0400

    fix[reduce.js]: fixed initialValue

--- array-methods-implementations-tasks/array-methods-implementations/reduce.js
+++ array-methods-implementations-tasks/array-methods-implementations/reduce.js
@@ -1,11 +1,18 @@
 Array.prototype.myReduce = function (callback, initialValue) {
-  let acc = initialValue ? initialValue : this[0];
-  const startIndex = initialValue ? 0 : 1;
+  let acc;
+  let startIndex;
+  if (initialValue === undefined) {
+    acc = array[0];
+    startIndex = 1;
+  } else {
+    startIndex = 0;
+    acc = initialValue;
+  }
   for (let index = startIndex; index < this.length; index++) {
     acc = callback(acc, this[index], index, this);
   }
   return acc;
 };
 const array = [1, 2, 3, 4, 20];
-const answer = array.myReduce((prev, current) => prev + current, 10);
+const answer = array.myReduce((prev, current) => prev + current);
 console.log(answer);
