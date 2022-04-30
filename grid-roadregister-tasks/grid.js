8804599ed3ce8b074881b62e5d6ca62a35501275
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-04-30 15:05:21 +0400

    feat[grid.js]: checkRow checkColumn

--- grid-roadregister-tasks/grid.js
+++ grid-roadregister-tasks/grid.js
@@ -17,33 +17,55 @@ function checkValidPuzzle(grid) {
     return false;
   }
 
-  if (checkValidGridLength(grid)) {
-    grid.forEach((row) => {
-      let rowNumbers = [];
+  console.log("checkValidGridLength", checkValidGridLength(grid));
 
-      row.map((item, index) => {
-        if (!isNaN(item)) {
-          if (row.lastIndexOf(item) > index) {
-            console.log(row[index]);
-          }
-          // rowNumbers.push(item);
-        }
+  function checkRow(grid) {
+    if (checkValidGridLength(grid)) {
+      const check = grid.map((row) => {
+        return row
+          .map((item, index) => {
+            if (!isNaN(item)) {
+              if (row.lastIndexOf(item) > index) {
+                console.log(row[index]);
+                return false;
+              }
+            }
+          })
+          .includes(false);
       });
-      // console.log("lastIndexOf", rowNumbers.lastIndexOf("2"));
-      console.log(rowNumbers);
-    });
+      if (check.includes(true)) {
+        return false;
+      } else {
+        return true;
+      }
+    }
   }
+  console.log("checkRow", checkRow(grid));
+
+  function checkColumn(grid) {
+    const result = [];
+    grid.forEach((row, rowIndex) => {
+      const temp = [];
+      row.forEach((column, columnIndex) => {
+        temp.push(grid[grid.length - columnIndex - 1][rowIndex]);
+      });
+      result.push(temp);
+    });
+
+    return checkRow(result);
+  }
+  console.log("checkColumn", checkColumn(grid));
 }
 console.log(
   checkValidPuzzle([
-    [".", ".", "1", "1", "4", ".", ".", "2", "2"],
+    [".", ".", ".", "1", "4", ".", ".", "2", "2"],
     [".", ".", "6", ".", ".", ".", ".", ".", "."],
     [".", ".", ".", ".", ".", ".", ".", ".", "."],
     [".", ".", "1", ".", ".", ".", ".", ".", "."],
     [".", "6", "7", ".", ".", ".", ".", ".", "9"],
-    [".", ".", ".", ".", ".", ".", "8", "1", "."],
+    [".", ".", ".", ".", ".", "8", ".", "1", "."],
     [".", "3", ".", ".", ".", ".", ".", ".", "6"],
-    [".", ".", ".", ".", ".", "7", "7", ".", "."],
+    [".", ".", ".", ".", ".", "7", ".", ".", "."],
     [".", ".", ".", "5", ".", ".", ".", "7", "."],
   ])
 );
