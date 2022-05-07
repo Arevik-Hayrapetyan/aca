45e74eff14d4150b0ab8b981c5f5d9eeef3996b9
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-05-07 12:36:09 +0400

    feat[chessPlayers]: add

--- chessPlayers-sort-tasks/chessPlayers.js
+++ chessPlayers-sort-tasks/chessPlayers.js
@@ -11,22 +11,20 @@ function getPlayers(playersCount, finishedMatches) {
   const allMatchesCount = getAllMatchesCount(playersCount);
   let count = 0;
   let step = playersCount;
-  let level = 1;
+  let level = 0;
   for (let i = 0; i < allMatchesCount / 2; i++) {
     for (let j = 1; j < step; j++) {
-      debugger;
-      let temp = [count, j];
+      let temp = [count, level + j];
       if (j === step - 1) {
         ++count;
         --step;
+        ++level;
       }
-      console.log(temp);
+      matrix.push(temp);
     }
-    // matrix.push(temp);
   }
   return matrix;
 }
-
 console.log(
   getPlayers(4, [
     [0, 1],
