13ee503b1c85ec7d1b9c15dafcdc99faf80cc774
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-05-07 17:32:14 +0400

    feat: chessPlayers task

--- chessPlayers-sort-tasks/chessPlayers.js
+++ chessPlayers-sort-tasks/chessPlayers.js
@@ -5,8 +5,40 @@ function getAllMatchesCount(playersCount) {
   }
   return acc;
 }
+function sortMatrix(matrix) {
+  let newMatrix = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] < matrix[i][1]) {
+      newMatrix.push(matrix[i]);
+    } else {
+      newMatrix.push([matrix[i][1], matrix[i][0]]);
+    }
+  }
+  newMatrix = newMatrix.sort(function (a, b) {
+    return a[0] - b[0];
+  });
+
+  return newMatrix;
+}
+function compareMatrixs(matrix, finishedMatches) {
+  for (let i = 0; i < finishedMatches.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (
+        finishedMatches[i][0] === matrix[j][0] &&
+        finishedMatches[i][1] === matrix[j][1]
+      ) {
+        matrix.splice(j, 1);
+      }
+    }
+  }
+  matrix = matrix.sort(function (a, b) {
+    return a[1] - b[1];
+  });
+  return matrix;
+}
 
 function getPlayers(playersCount, finishedMatches) {
+  finishedMatches = sortMatrix(finishedMatches);
   const matrix = [];
   const allMatchesCount = getAllMatchesCount(playersCount);
   let count = 0;
@@ -23,7 +55,7 @@ function getPlayers(playersCount, finishedMatches) {
       matrix.push(temp);
     }
   }
-  return matrix;
+  return compareMatrixs(matrix, finishedMatches);
 }
 console.log(
   getPlayers(4, [
