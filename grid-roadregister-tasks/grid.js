90df79c211c29757f658a13e37aeab46b431cd1e
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-05-07 17:54:02 +0400

    fix: fixed syntax

--- grid-roadregister-tasks/grid.js
+++ grid-roadregister-tasks/grid.js
@@ -1,67 +1,65 @@
-//BEM
-function checkValidPuzzle(grid) {
+function checkValidGridLength(grid) {
   const gridLength = 9;
   const rowLength = 9;
+  if (grid.length === gridLength) {
+    const checkedRowLength = grid.every((row) => {
+      return row.length == rowLength;
+    });
+    if (!checkedRowLength) {
+      console.log(`Invalid row length, it must be ${rowLength}`);
+      return false;
+    }
+    return true;
+  }
+  console.log(`Invalid grid length, it must be ${gridLength}`);
+  return false;
+}
 
-  function checkValidGridLength(grid) {
-    if (grid.length === gridLength) {
-      const checkedRowLength = grid.every((row) => {
-        return row.length == rowLength;
-      });
-      if (!checkedRowLength) {
-        console.log(`Invalid row length, it must be ${rowLength}`);
-        return false;
-      }
+function checkRow(grid) {
+  if (checkValidGridLength(grid)) {
+    const check = grid.map((row) => {
+      return row
+        .map((column, columnIndex) => {
+          if (!isNaN(column)) {
+            if (row.lastIndexOf(column) > columnIndex) {
+              return false;
+            }
+          }
+        })
+        .includes(false);
+    });
+    if (check.includes(true)) {
+      return false;
+    } else {
       return true;
     }
-    console.log(`Invalid grid length, it must be ${gridLength}`);
-    return false;
   }
+}
 
-  console.log("checkValidGridLength", checkValidGridLength(grid));
-
-  function checkRow(grid) {
-    if (checkValidGridLength(grid)) {
-      const check = grid.map((row) => {
-        return row
-          .map((column, columnIndex) => {
-            if (!isNaN(column)) {
-              if (row.lastIndexOf(column) > columnIndex) {
-                return false;
-              }
-            }
-          })
-          .includes(false);
-      });
-      if (check.includes(true)) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  }
-  console.log("checkRow", checkRow(grid));
-
-  function checkColumn(grid) {
-    const result = [];
-    grid.forEach((row, rowIndex) => {
-      const temp = [];
-      row.forEach((column, columnIndex) => {
-        temp.push(grid[columnIndex][rowIndex]);
-      });
-      result.push(temp);
+function checkColumn(grid) {
+  const result = [];
+  grid.forEach((row, rowIndex) => {
+    const temp = [];
+    row.forEach((column, columnIndex) => {
+      temp.push(grid[columnIndex][rowIndex]);
     });
+    result.push(temp);
+  });
 
-    return checkRow(result);
-  }
+  return checkRow(result);
+}
+
+function checkValidPuzzle(grid) {
+  console.log("checkValidGridLength", checkValidGridLength(grid));
+  console.log("checkRow", checkRow(grid));
   console.log("checkColumn", checkColumn(grid));
-
   if (checkRow(grid) && checkColumn(grid)) {
     return "Your puzzle is valid";
   } else {
     return "Your puzzle is invalid";
   }
 }
+
 console.log(
   checkValidPuzzle([
     [".", ".", ".", "1", "4", ".", ".", "2", "."],
